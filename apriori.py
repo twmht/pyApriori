b07da831d8b69fe24b117a83499211fcd42f94db
mht <qrnnis2623891@gmail.com>
2013-12-15 14:43:49 +0800

    update

--- apriori.py
+++ apriori.py
@@ -1,4 +1,5 @@
 import sys
+import csv
 
 from itertools import  combinations
 from collections import defaultdict
@@ -8,181 +9,164 @@ class Itemset(object):
     def __init__(self,items):
         self.items = items
         self.support = 0
+    def __equal__(self,other):
+        return self.items == other.items
+    def __len__(self):
+        return len(self.items)
 
 class TreeNode(object):
     def __init__(self):
         self.internalNode = dict()
+        """How many itemset share the same prefix"""
+        self.itemsets = []
 
-"""fast-join"""
-class PrefixTree(object):
-    def __init__(self):
-        self.root = dict()
-        self.leaf = []
-    def add(self,itemset):
-        node = self.root
-        for i in range(0,len(itemset.items)-1):
-            b = itemset.items[i]
-            if b in node.internalNode:
-                node = node.internalNode[b]
-            else:
-                n = TreeNode()
-                node.internalNode[b] = n
-                node = n
-        node.append(itemset)
-
-"""anti-monotone strategy"""
 class HashTree(object):
-    def __init__(self,fp = True):
-        self.leaf = []
+    def __init__(self):
         self.root = TreeNode()
-        self.fp = fp;
+        self.itemset = None
+        """how many leaf nodes"""
+        self.leaf = []
+        self.length = 0
+
+    def __len__(self):
+        return self.length
+
     def add(self,itemset):
-        if self.fp == True:
-            items = itemset.items[0:len(itemset.items)-1]
-        else:
-            items = itemset.items
         node = self.root
-        for item in items:
+        for item in range(0,len(itemset)-1):
             if item in node.internalNode:
                 node = node.internalNode[item]
             else:
                 n = TreeNode()
                 node.internalNode[item] = n
                 node = n
-        node.leaf.append(items)
-    def update(self,itemset):
+        """prefix leaf nodes"""
+        if(len(self.leaf) == 0):
+            """How many leaf nodes do we have"""
+            self.leaf.append(node)
+        node.itemsets.append(itemset)
+        if itemset[-1] in node.internalNode:
+            node = node.internalNode[itemset[-1]]
+        else:
+            n = TreeNode()
+            node.internalNode[item] = n
+        """Number of itemset is incremented"""
+        n.itemset = itemset
+        self.length = self.length+1
+
+    def update(self,itemset,freqSet):
         node = self.root;
         for item in itemset:
             if item in node.internalNode:
-                node = node.internalNode
+                node = node.internalNode[item]
             else:
                 return False
-        node.leaf[0] = node.leaf[0]+1
+        node.itemset = node.itemset.support+1
+
+        global transactions,minSupport
+        if node.itemset.suppot/len(transactions) == minSupport:
+            freqSet.add(node.itemset)
+
+    def exist(self,itemset):
+        node = self.root
+        for item in itemset.items:
+            if item in node.internalNode:
+                node = node.internalNode[item]
+            else:
+                return False
+        return True
 
 
+def antiMonotonePruning(itemset,a,b,freqSet):
+    """Every subset in a frequent itemset must be frequent"""
+    for c in combinations(itemset.items,len(freqSet)-1):
+        if c != a and c != b:
+            if not freqSet.exist(c):
+                return False
+    return True
 
-def subsets(itemset,k):
-    """ Returns non empty subsets of arr"""
-    return combinations(itemset,k-1)
+def returnItemsWithMinSupport(candidateSet, k):
+    global transactions
+    freqSet = HashTree()
+    for t in transactions:
+        for c in combinations(t,k):
+            candidateSet.update(itemset(c),freqSet)
+    return freqSet
 
+def joinSet(largeSet,k):
+    candidate = HashTree()
+    if k == 2:
+        """largeSet is a list"""
+        for c in combinations(largeSet,2):
+            candidate.add(c)
+        return candidate
+    """largeSet is a HashTree"""
+    for leaf in largeSet.leafs:
+        for i in range(0,len(leaf.itemsets)):
+            for j in range(i+1,len(leaf.itemsets)) :
+                if leaf.itemsets[i].items[k-1]>leaf.itemsets[j].items[k-1]:
+                    a = leaf.itemset[i].items
+                    b = leaf.itemset[j].items
+                    r = itemset(a[0:k-1]+[b[k-1],a[k-1]])
+                    if antiMonotonePruning(r,a,b,freqSet) == True:
+                        candidate.add(r)
+    return candidate
 
-def antiMonotonePruning():
-    pass
-def returnItemsWithMinSupport(transactionList,freqSet, candidates, minSupport,k):
-    if k > 2:
-        antiMonotonePruning(freqSet,candidates,k)
-    newFreqSet = HashTree(fp = True)
-    for transaction in transactionList:
-        for subset in subsets(transaction,k+1):
-            candidates.update(subset)
-
-
-def joinSet(tree,k):
-    """Join a set with itself and returns the n-element itemsets"""
-    candidates = HashTree()
-    for leaf in tree.leaf:
-        for i in range(0,len(leaf)):
-            for j in range(i+1,len(leaf)):
-                if leaf[i][-1]>leaf[j][-1]:
-                    items = [item for item in leaf[i][0:k-1]]
-                    items.append(leaf[j][-1])
-                    items.append(leaf[i][-1])
-                else:
-                    items = [item for item in leaf[i][0:k-1]]
-                    items.append(leaf[i][-1])
-                    items.append(leaf[j][-1])
-    return candidates
-
-
-def initial(data_iterator,minSupport):
+def firstPass():
+    global transactions,minSupport
     """large 1-itemset and 2-candidates"""
     itemset = dict(int)
-    for record in data_iterator:
-        for item in record:
+    for trans in transactions:
+        for item in trans:
             itemset[item] = itemset[item]+1
-    largeset = []
+    largeSet = []
     for key,value in itemset.items():
         if value >= minSupport:
-            largeset.append(key)
+            largeSet.append(key)
+    largeSet.sort()
+    return largeSet
 
-    candidates = HashTree(fp = False)
-    for i in range(0,len(largeset)):
-        for j in range(i+1,len(largeset)):
-            candidates.add(Itemset([largeset[i],largeset[j]]))
-    return large,candidates
-
-def runApriori(data_iter, minSupport, minConfidence):
-    """the large-1 itemset and candidates-2 itemset"""
-    currentLSet,currentCSet = initial(data_iter,minSuppor)
+def runApriori():
+    """the large-1 itemset"""
+    global freqDict
+    currentLSet = firstPass()
     assocRules= dict()
     k = 2
-    while(currentLSet != set([])):
-        largeSet[k-1]= currentLSet
-        currentLSet= returnItemsWithMinSupport(currentLSet, currentCSet, minSupport)
-        currentCSet = joinSet(currentLSet)
+    while(len(currentLSet)>1):
+        freqDict[k-1]= currentLSet
+        currentCSet = joinSet(currentLSet,k)
+        currentLSet= returnItemsWithMinSupport(currentLSet, currentCSet)
         k = k + 1
 
-    def getSupport(item):
-        """local function which Returns the support of an item"""
-        return float(freqSet[item])/len(transactionList)
 
-    toRetItems=[]
-    for key,value in largeSet.items():
-        toRetItems.extend([(tuple(item), getSupport(item))
-            for item in value])
-
-        toRetRules=[]
-    for key,value in largeSet.items()[1:]:
-        for item in value:
-            _subsets = map(frozenset,[x for x in subsets(item)])
-            for element in _subsets:
-                remain = item.difference(element)
-                if len(remain)>0:
-                    confidence = getSupport(item)/getSupport(element)
-                    if confidence >= minConfidence:
-                        toRetRules.append(((tuple(element),tuple(remain)),
-                            confidence))
-                        return toRetItems, toRetRules
-
-
-def printResults(items, rules):
-    """prints the generated itemsets and the confidence rules"""
-    for item, support in items:
-        print "item: %s , %.3f" % (str(item), support)
-    print "\n------------------------ RULES:"
-    for rule, confidence in rules:
-        pre, post = rule
-        print "Rule: %s ==> %s , %.3f" % (str(pre), str(post), confidence)
-
-
-def dataFromFile(fname):
-    """Function which reads from the file and yields a generator"""
-    file_iter = open(fname, 'rU')
-    for line in file_iter:
-        record = (map(int,line.split(',')))
-        record.sort()
-        yield record
-
+def readCVSfile(infile):
+    global transactions
+    for trans in infile:
+        t = trans[1:]
+        t.sort()
+        transactions.append(t)
 
 if __name__ == "__main__":
 
+    global minSupport,minConfidence,transactions,freqDict
+    transactions = []
+    freqDict = dict()
+
     optparser = OptionParser()
-    optparser.add_option('-f', '--inputFile', dest = 'input', help = 'the filename which contains the comma separated values', default=None)
-    optparser.add_option('-s', '--minSupport', dest='minS', help = 'minimum support value', default=0.15, type='float')
-    optparser.add_option('-c','--minConfidence', dest='minC', help = 'minimum confidence value', default = 0.6, type='float')
+    optparser.add_option('-f', '--inputFile', dest = 'input', help = 'the filename which contains the comma separated values')
+    optparser.add_option('-s', '--minSupport', dest='minS', help = 'minimum support value(default=0.15)', default=0.15, type='float')
+    optparser.add_option('-c','--minConfidence', dest='minC', help = 'minimum confidence value(default = 0.6)', default = 0.6, type='float')
 
     (options, args) = optparser.parse_args()
 
-    if options.input is None:
-        inFile = sys.stdin
-    elif options.input is not None:
-        inFile = dataFromFile(options.input)
+    if options.input is not None:
+        readCVSfile(csv.reader(open(options.input,'r')))
     else:
         print 'No dataset filename specified, system with exit\n'
         sys.exit('System will exit')
 
     minSupport		= options.minS
     minConfidence = options.minC
-    items, rules	= runApriori(inFile, minSupport, minConfidence)
+    runApriori()
 
     printResults(items,rules)
