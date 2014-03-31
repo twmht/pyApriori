b73898f20b0355cb71e52a8f6dfb35421f5b03b3
mht <qrnnis2623891@gmail.com>
2014-03-31 21:36:29 +0800

    frequent itemset

--- apriori.py
+++ apriori.py
@@ -17,6 +17,15 @@ class Itemset(object):
         return len(self.items)
     def __getitem__(self,key):
         return self.items[key]
+    def __iter__(self):
+        self.begin = 0
+        return self
+    def next(self):
+        if self.begin >=  len(self.items):
+            raise StopIteration
+        self.begin = self.begin+1
+        return self.items[self.begin-1]
+
 
 class TreeNode(object):
     def __init__(self):
@@ -36,30 +45,32 @@ class HashTree(object):
         self.length = k
 
     def __len__(self):
-        return self.length
+        return len(self.itemsets)
 
     def add(self,itemset):
         node = self.root
         new = False
-        for item in range(0,len(itemset)-1):
-            if item in node.internalNode:
-                node = node.internalNode[item]
+        for item in range(0,self.length-1):
+            if itemset[item] in node.internalNode:
+                node = node.internalNode[itemset[item]]
             else:
                 new = True
                 n = TreeNode()
-                node.internalNode[item] = n
+                node.internalNode[itemset[item]] = n = TreeNode()
                 node = n
         """prefix leaf nodes"""
         """How many leaf nodes do we have"""
         if new == True:
+            """This node is a new leaf node"""
             self.leafs.append(node)
         node.itemsets.append(itemset)
-        """for antiMonotonePruning"""
-        n = TreeNode()
-        node.internalNode[itemset[-1]] = n
+        """the total itemsets"""
+        self.itemsets.append(itemset)
+        """A fake leaf node for antiMonotonePruning"""
+        node.internalNode[itemset[-1]] = n =  TreeNode()
         n.itemset = itemset
-        self.itemsets.append(n.itemset)
 
+    """update the support count and find out the frequent itemset"""
     def update(self,itemset,freqSet):
         node = self.root;
         for item in itemset:
@@ -68,14 +79,13 @@ class HashTree(object):
             else:
                 return False
         node.itemset.support = node.itemset.support+1
-
         global transactions,minSupport
-        if node.itemset.support == minSupport*len(transactions):
+        if node.itemset.support == int(minSupport*len(transactions)):
             freqSet.add(node.itemset)
 
-    def exist(self,itemset):
+    def exist(self,items):
         node = self.root
-        for item in itemset.items:
+        for item in items:
             if item in node.internalNode:
                 node = node.internalNode[item]
             else:
@@ -85,7 +95,7 @@ class HashTree(object):
 
 def antiMonotonePruning(itemset,a,b,freqSet):
     """Every (k-1)-subset in a k-frequent itemset must be frequent"""
-    for c in combinations(itemset.items,len(freqSet)-1):
+    for c in combinations(itemset.items,freqSet.length):
         if c != a and c != b:
             if not freqSet.exist(c):
                 return False
@@ -101,23 +111,23 @@ def returnItemsWithMinSupport(candidateSet, k):
 
 def joinSet(largeSet,k):
     candidate = HashTree(k)
-    print k
     if k == 2:
         """largeSet is a list"""
-        for c in combinations(largeSet,2):
+        for c in combinations(largeSet.iterkeys(),2):
             candidate.add(Itemset(c))
         return candidate
     """largeSet is a HashTree"""
     for leaf in largeSet.leafs:
         for i in range(0,len(leaf.itemsets)):
             for j in range(i+1,len(leaf.itemsets)) :
-                a = leaf.itemset[i].items
-                b = leaf.itemset[j].items
-                if a[k-1] > b[k-1]:
-                    r = itemset(a[0:k-1]+[b[k-1],a[k-1]])
+                a = leaf.itemsets[i]
+                b = leaf.itemsets[j]
+                if a[k-2] > b[k-2]:
+                    r = Itemset(a[0:k-2]+(b[k-2],a[k-2]))
                 else:
-                    r = itemset(a[0:k-1]+[a[k-1],b[k-1]])
-                if antiMonotonePruning(r,a,b,freqSet) == True:
+                    r = Itemset(a[0:k-2]+(a[k-2],b[k-2]))
+                if antiMonotonePruning(r,a,b,largeSet) == True:
+                    #print r.items
                     candidate.add(r)
     return candidate
 
@@ -128,19 +138,25 @@ def firstPass():
     for trans in transactions:
         for item in trans:
             itemset[item] = itemset[item]+1
-    largeSet = []
+    largeSet = defaultdict(int)
     for key,value in itemset.items():
-        if value >= minSupport*len(transactions):
-            largeSet.append(key)
-    largeSet.sort()
+        if value >= int(minSupport*len(transactions)):
+            largeSet[key] = value
     return largeSet
 
 def findMaximal(currentLSet,lastLSet):
-    for items in currentLSet.itemsets:
-        for c in combinations(items,len(lastLSet)):
-            itemset = lastLSet.exist(c)
-            if itemset != False:
-                itemset.maximal = True
+    if currentLSet.length == 2:
+        for itemset in currentLSet.itemsets:
+            a = itemset[0]
+            b = itemset[1]
+
+    else:
+        for items in currentLSet.itemsets:
+            for c in combinations(items,lastLSet.length):
+                itemset = lastLSet.exist(c)
+                """if the itemset is not maximal"""
+                if itemset != False:
+                    itemset.maximal = False
 
 def runApriori():
     """the large-1 itemset"""
@@ -148,7 +164,7 @@ def runApriori():
     currentLSet = firstPass()
     assocRules= dict()
     k = 2
-    while(len(currentLSet)>1):
+    while(len(currentLSet) >= 1):
         freqDict[k-1]= currentLSet
         currentCSet = joinSet(currentLSet,k)
         currentLSet= returnItemsWithMinSupport(currentCSet,k)
@@ -158,7 +174,6 @@ def runApriori():
 
 def readCVSfile(infile):
     global transactions
-    print (infile.line_num)
     for trans in infile:
         t = map(int,trans[1:])
         t.sort()
@@ -167,14 +182,19 @@ def readCVSfile(infile):
 def readGoods(infile):
     global goods
     for good in infile:
-        goods[int(good[0])] = good[1]
+        goods[int(good[0])] = good[1].replace("'","") +' ' + good[2].replace("'","")
+
+def printRules():
+    pass
 
 def printFrequentItemsets():
-    global freqDict,goods
+    global freqDict,goods,transactions
     for key,value in freqDict.iteritems():
-        for itemset in value.itemsets:
-            items = ','.join(goods[item] for item in itemset.items)
-            print items
+        if key >= 2:
+            for itemset in value.itemsets:
+                if itemset.maximal == True:
+                    items = ', '.join(goods[item] for item in itemset.items)
+                    print items,float(itemset.support)/len(transactions)
 
 if __name__ == "__main__":
 
@@ -198,7 +218,7 @@ if __name__ == "__main__":
         sys.exit('System will exit')
 
     if options.good is not None:
-        readGoods(cvs.reader(open(options.good,'r')))
+        readGoods(csv.reader(open(options.good,'r')))
 
     minSupport = options.minS
     minConfidence = options.minC
